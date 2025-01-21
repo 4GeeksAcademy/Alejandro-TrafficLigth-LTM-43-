c131bea9410a4a2d5df66d1ac598bc910e079336
Alejandro Arraga <alejandroarraga99@gmail.com>
2025-01-21 02:57:37 +0000

    added ternari

--- src/js/components/trafficLight.jsx
+++ src/js/components/trafficLight.jsx
@@ -1,14 +1,29 @@
-import React from "react"
-
+import React, { useState } from "react"
 
 const trafficLight = () => {
+
+    const [selected, setSelected] = useState("yellow")
+    
+    const changeColor = (color) => {
+        setSelected(color)
+    }
+    
     return (
         <main>
             <div className="support-box"></div>
             <div className="lights-boxes">
-                <div className="red lights-one"></div>
-                <div className="yellow"></div>
-                <div className="green"></div>
+                <div 
+                    className={'red ${selected == "red" ? "on" : ""}'} 
+                    onClick={()=>changeColor("red")}>                
+                </div>
+                <div
+                    className={'yellow ${selected == "yellow" ? "on" : ""}'}
+                    onClick={()=>changeColor("yellow")}>
+                </div>
+                <div
+                    className={'green ${selected == "green" ? "on" : ""}'}
+                    onClick={()=>changeColor("green")}>
+                </div>
             </div>    
         </main>
     )
