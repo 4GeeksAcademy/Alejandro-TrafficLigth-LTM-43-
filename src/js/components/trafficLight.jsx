492fed7259f3d3adcd4bed7530810d9b602b45b0
Alejandro Arraga <alejandroarraga99@gmail.com>
2025-01-21 01:05:45 +0000

    added styles

---
+++ src/js/components/trafficLight.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+const trafficLight = () => {
+    return (
+        <main>
+            <div className="support-box"></div>
+            <div className="lights-boxes">
+                <div className="red lights-one"></div>
+                <div className="yellow"></div>
+                <div className="green"></div>
+            </div>    
+        </main>
+    )
+}
+export default trafficLight
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,10 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import TrafficLight from './components/trafficLight';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <TrafficLight/>
   </React.StrictMode>,
 )
